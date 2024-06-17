b5c895d17503bd91ae6bfecb034db83aa79efb8c
Rishabh416 <mittalr2008@gmail.com>
2024-06-17 18:38:38 +0400

    added webui

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
-from fastapi import FastAPI, Response
-from picamera2 import Picamera2
-import cv2
+from fastapi import FastAPI  
+from fastapi.responses import StreamingResponse  
+import uvicorn 
+from picamera2 import Picamera2, Preview  
+import io  
+import cv2 
+import time
 
 app = FastAPI()
 
-# Initialize the camera
 picam2 = Picamera2()
 config = picam2.create_preview_configuration(main={"size": (640, 480)})
-picam2.configure(config)
-picam2.start()
+picam2.configure(config)  
+picam2.start()  
 
-@app.get("/video_feed")
-async def video_feed():
-    # Capture a single frame as a numpy array
-    frame = picam2.capture_array()
-    # Encode the frame as JPEG
-    ret, buffer = cv2.imencode('.jpg', frame)
-    # Create a response with the JPEG image
-    return Response(content=buffer.tobytes(), media_type="image/jpeg")
+def generate_frames():
+    while True:  
+        frame = picam2.capture_array()  
+        ret, buffer = cv2.imencode('.jpg', frame)  
+        frame = buffer.tobytes()  
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.get("/videoFeed")
+async def videoFeed():
+    return StreamingResponse(generate_frames(), media_type="multipart/x-mixed-replace; boundary=frame")
+
+@app.get("/takeImage")
+async def takeImage():
+    picam2.capture_file(f"{time.time()}.jpg")
+    print("Taken image")
+    return("captured")
 
 if __name__ == "__main__":
-    import uvicorn  # Import uvicorn for running the FastAPI app
-    # Run the app on all available IP addresses (0.0.0.0) at port 8000
     uvicorn.run(app, host="0.0.0.0", port=8000)
