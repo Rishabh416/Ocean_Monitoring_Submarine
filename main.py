6a5acfa4bbe3ecc4feef16bf76f8c2b7c6b29cfc
rishabh416 <mittalr2008@gmail.com>
2024-06-21 16:16:12 +0400

    Updated documentation

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from fastapi import FastAPI  
-from fastapi.responses import StreamingResponse  
-from fastapi.middleware.cors import CORSMiddleware
-import uvicorn 
-from picamera2 import Picamera2, Preview  
-import io  
-import cv2 
-import time
+from fastapi import FastAPI  # API endpoint import
+from fastapi.responses import StreamingResponse # live streaming import
+from fastapi.middleware.cors import CORSMiddleware # Cross-origin resource sharing policy override import
+import uvicorn # live server/API host library
+from picamera2 import Picamera2  # camera interface library
+import cv2 # image processing library
+import time # current time details library
 
+# initialize API
 app = FastAPI()
 
+# override CORS policy on browsers
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -17,28 +18,33 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-picam2 = Picamera2()
-config = picam2.create_preview_configuration(main={"size": (640, 480)})
-picam2.configure(config)  
-picam2.start()  
+# initialize camera and set its configuration (480p resolution)
+cam = Picamera2()
+config = cam.create_preview_configuration(main={"size": (640, 480)})
+cam.configure(config)  
+cam.start()  
 
+# function to get current frame, encode as jpg and convert it into bytes
 def generate_frames():
     while True:  
-        frame = picam2.capture_array()  
+        frame = cam.capture_array()  
         ret, buffer = cv2.imencode('.jpg', frame)  
         frame = buffer.tobytes()  
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+# API endpoint for live video streaming from camera
 @app.get("/videoFeed")
 async def videoFeed():
     return StreamingResponse(generate_frames(), media_type="multipart/x-mixed-replace; boundary=frame")
 
+# API endpoint for capturing current image of camera, name of image is set to current timestamp
 @app.get("/takeImage")
 async def takeImage():
-    picam2.capture_file(f"{time.time()}.jpg")
+    cam.capture_file(f"{time.time()}.jpg")
     return("captured")
 
+# following set of functions are for controlling the movement of a submarine
 @app.get("/forward")
 async def forward():
     print("forward") # replace with movement forward code
@@ -74,5 +80,6 @@ async def stop():
     print("stop") # replace with movement stop code
     return("stop")    
 
+# run the API on a localhost server at port 8000
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- readme.md
+++ readme.md
@@ -1,7 +1,13 @@
 # Research Project with Rochester Institute of Technology, Dubai
-This project works on creating a ocean monitoring submarine. 
+This project works on creating a ocean monitoring submarine. The system is equipped with a Raspberry Pi 4 Model B which is able to capture live video feed through an Arducam 5MP camera. The Raspberry Pi uses the Picamera2 library to interface with the camera. The live video feed is displayed on a webUI which also facilitates a 9-key control system to maneuver the submarine. 
 
 ### Hardware
 Raspberry Pi 4 Model B
 Arducam 5MP OV5647
 
+### Software
+- FastAPI: This library is used to provide API access points that can be accessed by the webUI to display a live camera feed and relay information
+- Uvicorn: This library is a web server which is being used to host the API endpoints on a local host. 
+- Picamera2: This library is used to interface with the camera and capture images.
+- OpenCV: This library is for image processing and used to encode/decode the images so that it can be relayed over an API endpoint. 
+- Time: This library is used to get access to the current time and provide a unique identifier to each image saved. 
